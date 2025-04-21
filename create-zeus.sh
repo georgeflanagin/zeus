b9a6043f804394c287a903a1e5a1ec7b76e238e5
georgeflanagin <milesdavis@billevans>
2025-04-21 12:51:35 -0400

    Updated create-zeus to include tmp file system.

--- create-zeus.sh
+++ create-zeus.sh
@@ -26,6 +26,8 @@ getent group "$TRUSTEE_GROUP" >/dev/null || groupadd "$TRUSTEE_GROUP"
 # Create zeus user if missing
 if ! id "$ZEUS_USER" &>/dev/null; then
     useradd -m -s /bin/bash -g "$ZEUS_GROUP" -g "$TRUSTEE_GROUP" "$ZEUS_USER"
+    chown root:trustee /home/zeus
+    chmod 0640 /home/zeus
     echo "Created user '$ZEUS_USER'"
 fi
 
